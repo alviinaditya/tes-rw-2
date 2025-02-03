dce06342bc043ff7c0b5c98f73981090b910248f
Alvin A. Rahman <alviinaditya@gmail.com>
2025-02-03 10:46:21 +0700

    refactor: dashboard routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,46 +7,62 @@ const router = createRouter({
     {
       path: "/",
       name: "login",
-      component: () => import("../views/auth/LoginView.vue"),
+      component: () => import("@/views/auth/LoginView.vue"),
     },
     {
       path: "/dashboard",
       name: "dashboard",
-      component: () => import("../views/dashboard/DashboardView.vue"),
-    },
-    {
-      path: "/data",
-      name: "data",
-      component: () => import("../views/dashboard/data/ListDataView.vue"),
-    },
-    {
-      path: "/data/create",
-      name: "data-create",
-      component: () => import("../views/dashboard/data/CreateDataView.vue"),
-    },
-    {
-      path: "/data/update/:id",
-      name: "data-update",
-      component: () => import("../views/dashboard/data/UpdateDataView.vue"),
-    },
-    {
-      path: "/categories",
-      name: "categories",
-      component: () =>
-        import("../views/dashboard/category/ListCategoryView.vue"),
-    },
-    {
-      path: "/categories/create",
-      name: "categories-create",
-      component: () =>
-        import("../views/dashboard/category/CreateCategoryView.vue"),
-    },
-    {
-      path: "/qa",
-      name: "qa",
-      component: () => import("../views/dashboard/qa/QAView.vue"),
+      component: () => import("@/views/dashboard/DashboardView.vue"),
+      meta: { requiresAuth: true },
+      children: [
+        {
+          path: "",
+          name: "departments",
+          component: () =>
+            import("@/views/dashboard/department/ListDepartmentView.vue"),
+        },
+        {
+          path: "/data",
+          name: "data",
+          component: () => import("@/views/dashboard/data/ListDataView.vue"),
+        },
+        {
+          path: "/data/create",
+          name: "createData",
+          component: () => import("@/views/dashboard/data/CreateDataView.vue"),
+        },
+        {
+          path: "/data/update/:id",
+          name: "updateData",
+          component: () => import("@/views/dashboard/data/UpdateDataView.vue"),
+        },
+        {
+          path: "/categories",
+          name: "categories",
+          component: () =>
+            import("@/views/dashboard/category/ListCategoryView.vue"),
+        },
+        {
+          path: "/categories/create",
+          name: "createCategory",
+          component: () =>
+            import("@/views/dashboard/category/CreateCategoryView.vue"),
+        },
+        {
+          path: "/qa",
+          name: "qa",
+          component: () => import("@/views/dashboard/qa/QAView.vue"),
+        },
+      ],
     },
   ],
+});
+
+router.beforeEach((to) => {
+  const store = useAuthStore();
+  if (to.meta.requiresAuth && !store.isAuthenticated) {
+    return { name: "login" };
+  }
 });
 
 export default router;
